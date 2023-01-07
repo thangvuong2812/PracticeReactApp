c9bb362b3b6cad2e69e216d5b8f0b4cdc6fcb2cf
thangvuong2812 <thangvuong2812@gmail.com>
2023-01-07 12:28:36 +0700

    init

---
+++ anttest/src/features/Todo/components/TodoForm/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const TodoForm = props => {
+  return (
+    <div>TodoForm</div>
+  )
+}
+
+TodoForm.propTypes = {}
+
+export default TodoForm
